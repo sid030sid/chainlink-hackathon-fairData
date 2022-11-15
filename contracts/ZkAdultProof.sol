d4ea6499945212325b5ed30f5862e5311f06d0c4
sid030sid <sid.lam@gmx.de>
2022-11-15 15:45:43 +0100

    optimized ZkAdultProof contract + update fo todo

--- contracts/ZkAdultProof.sol
+++ contracts/ZkAdultProof.sol
@@ -16,7 +16,7 @@ contract ZkAdultProof is ChainlinkClient, ConfirmedOwner {
     constructor() ConfirmedOwner(msg.sender) {
         setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
         setChainlinkOracle(0xCC79157eb46F5624204f47AB42b3906cAA40eaB7); //chainlink's testnet oracle on goerli 
-        jobId= "ca98366cc7314957b8c012c72f05aeeb";
+        jobId = "ca98366cc7314957b8c012c72f05aeeb";
         fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
     }
 
@@ -33,15 +33,6 @@ contract ZkAdultProof is ChainlinkClient, ConfirmedOwner {
     //note: birthday is uint as it is in unix epoch format
     function fulfillZkProofAdulthood (bytes32 _requestId, uint _birthday) public recordChainlinkFulfillment(_requestId) {
         if(_birthday + 18 * 365 days > block.timestamp){
-            emit AdulthoodProof(true);
-        }else{
-            emit AdulthoodProof(false);
-        }
-    }
-
-    //_birthday should be in unix epoch format format (=seconds after 1970-01-01 till _birthday date)
-    function checkAdulthood(uint _birthday) public {
-        if(uint(_birthday) + 18 * 365 days > block.timestamp){
             emit AdulthoodProof(true);
         }else{
             emit AdulthoodProof(false);
